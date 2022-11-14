0cac0e2583a14677ae2ba142f375c860477169d0
ALCroke <85999910+ALCroke@users.noreply.github.com>
2022-11-14 16:05:34 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Script Loaded');
+var signUpForm = document.querySelector('#signUpForm');
+var userNameField = document.querySelector('usernamefield');
+var useereNameErrorMessage = document.querySelector('#usernameErrorMessage');
+
+signUpForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    if(!signUpForm.username.value == false){
+
+        usernamefield.classList.add('errorField');
+        usereNameErrorMessage.classList.remove('hidden');
+     console.log('You need to enter a username');
+
+    }
+});
